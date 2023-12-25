1e614f9699340c1a757c1cbd3d2c5f39bb56fec7
Priyom1 <soikatmirror23@gmail.com>
2023-12-25 20:04:26 +0600

    boxdesign

--- lib/comp/my_button.dart
+++ lib/comp/my_button.dart
@@ -6,11 +6,20 @@ class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(color: Colors.black87),
+      padding: EdgeInsets.all(30),
+      margin: EdgeInsets.symmetric(horizontal: 25),
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(10),
+        ),
       child: Center(
         child: Text(
           "Sign in",
-          style: TextStyle(color: Colors.white70),
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+            ),
         ),
       ),
     );
