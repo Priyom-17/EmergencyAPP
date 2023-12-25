299910ce2fb86c70da31ef74252d55a5606bf285
Priyom1 <soikatmirror23@gmail.com>
2023-12-25 20:07:49 +0600

    GestureDetector

--- lib/comp/my_button.dart
+++ lib/comp/my_button.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-  const MyButton({super.key});
+  final Function()?onTap;
+  const MyButton({super.key,required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(30),
-      margin: EdgeInsets.symmetric(horizontal: 25),
-      decoration: BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.circular(10),
-        ),
-      child: Center(
-        child: Text(
-          "Sign in",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 18,
-            ),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(30),
+        margin: EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(10),
+          ),
+        child: Center(
+          child: Text(
+            "Sign in",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+              ),
+          ),
         ),
       ),
     );
