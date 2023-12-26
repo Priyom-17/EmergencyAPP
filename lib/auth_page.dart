f192340ead25fd9993bcc1d9e52684f4a23a984a
Priyom1 <soikatmirror23@gmail.com>
2023-12-27 01:31:31 +0600

    imports

--- lib/auth_page.dart
+++ lib/auth_page.dart
@@ -1,6 +1,7 @@
 import 'package:emergency_app/login.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:emergency_app/HomePage.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
