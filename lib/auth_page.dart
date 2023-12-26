d45a73e384efc679aeac8c1a08051f96dc9820b4
Priyom1 <soikatmirror23@gmail.com>
2023-12-27 01:28:19 +0600

    Created Homepage

--- lib/auth_page.dart
+++ lib/auth_page.dart
@@ -1,10 +1,27 @@
+import 'package:emergency_app/login.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          //User logged in
+          if(snapshot.hasData){
+            return HomePage();
+          }
+          //User not logged in
+          else{
+            return LoginPage();
+          }
+
+        },
+      )
+    );
   }
 }
