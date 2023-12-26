a19bd2b77ed8c1dbf833f21904f859f64cde8765
Priyom1 <soikatmirror23@gmail.com>
2023-12-27 02:15:44 +0600

    logout button

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,11 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  //signout method
+  void signOutUser() {
+    FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: signOutUser,
+             icon: Icon(
+              Icons.logout,
+             )
+            )
+        ],
+      ),
       body: Center(
         child: Text("Logged In")
         ),
