c34500f1435b3834b91610b2db0fbf7184905e1d
Priyom1 <soikatmirror23@gmail.com>
2024-01-06 13:57:03 +0600

    wrong cred alert

--- lib/login.dart
+++ lib/login.dart
@@ -3,11 +3,13 @@ import 'package:emergency_app/comp/my_textfield.dart';
 import 'package:emergency_app/comp/square_tile.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:emergency_app/HomePage.dart';
 
 
 
 class LoginPage extends StatefulWidget {
-   LoginPage({super.key});
+  const LoginPage({super.key});
 
   @override
   State<LoginPage> createState() => _LoginPageState();
@@ -33,10 +35,40 @@ class _LoginPageState extends State<LoginPage> {
 
 
     //try sign in
-    await FirebaseAuth.instance.signInWithEmailAndPassword(
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
       email: usernameController.text, 
       password: passwordController.text,
       );
+      //pop navigator
+      Navigator.pop(context);
+    } on FirebaseAuthException catch(e) {
+      //pop navigator
+      Navigator.pop(context);
+      showErrorMessage(e.code);
+    }
+    
+  }
+
+  //error message
+  void showErrorMessage(String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: Colors.deepPurple,
+          title: Center(
+            child: Text(
+              message,
+              style: const TextStyle(
+                color: Colors.white
+              ),
+            ),
+          ),
+        );
+
+      },
+    );
   }
 
   @override
@@ -45,138 +77,140 @@ class _LoginPageState extends State<LoginPage> {
       backgroundColor: Colors.grey.shade300,
       body: SafeArea(
         child: Center(
-          child: Column(
-            //mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 10),
-              //logo
-              const Icon(
-               Icons.account_circle,
-               size: 80, 
-
-              ),
-              const SizedBox(height: 10),
-              //Welcome Text
-              const Text(
-                'Welcome',
-                style: TextStyle(
-                  color: Colors.black87,
-                  fontSize: 28,
-                  fontWeight: FontWeight.w800,
-
+          child: SingleChildScrollView(
+            child: Column(
+              //mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 10),
+                //logo
+                const Icon(
+                 Icons.account_circle,
+                 size: 80, 
+            
                 ),
-              ),
-
-              const SizedBox(height: 20),
-
-              //username TextField
-              MyTextField(
-                controller: usernameController,
-                hintText: 'Username',
-                obsecureText: false,
-
-
-              ),
-              const SizedBox(height: 15),
-
-              //password Textfield
-              MyTextField(
-                controller: passwordController,
-                hintText: 'Password',
-                obsecureText: true,
-              ),
-              const SizedBox(height: 15),
-
-              //forgot password?
-              const Text(
-                'Forgot password?',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 4, 102, 84),
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15
+                const SizedBox(height: 10),
+                //Welcome Text
+                const Text(
+                  'Welcome',
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 28,
+                    fontWeight: FontWeight.w800,
+            
                   ),
                 ),
+            
                 const SizedBox(height: 20),
-
-                MyButton(
-                  onTap: signInUser,
+            
+                //username TextField
+                MyTextField(
+                  controller: usernameController,
+                  hintText: 'Username',
+                  obsecureText: false,
+            
+            
                 ),
-                /*ElevatedButton(onPressed: (){
-                  signInUser();
-                }, 
-                child: Text("Login")),*/
-                const SizedBox(height: 40),
-
-                Row(
-                  children: [
-                    Expanded(
-                      child: Divider(
-                        thickness: 1.2,
-                        color: Colors.grey[600],
-                      ),
-                      ),
+                const SizedBox(height: 15),
+            
+                //password Textfield
+                MyTextField(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  obsecureText: true,
+                ),
+                const SizedBox(height: 15),
+            
+                //forgot password?
+                const Text(
+                  'Forgot password?',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 4, 102, 84),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+            
+                  MyButton(
+                    onTap: signInUser,
+                  ),
+                  /*ElevatedButton(onPressed: (){
+                    signInUser();
+                  }, 
+                  child: Text("Login")),*/
+                  const SizedBox(height: 40),
+            
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 1.2,
+                          color: Colors.grey[600],
+                        ),
+                        ),
+                        Text(
+                          'Or Continue with',
+                          style: TextStyle(
+                            color: Colors.grey.shade600,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20
+                          ),
+                          ),
+                        const Expanded(
+                          child: Divider(
+                            thickness: 1.2,
+                            color: Colors.grey,
+                          ),
+                          ),
+                    ],
+                  ),
+                  const SizedBox(height: 30),
+                  //google and apple image
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      //google button
+                      SquareTile(imagepath: 'images/google.png'),
+            
+                      SizedBox(width: 20),
+            
+                      //apple image
+                      SquareTile(imagepath: 'images/facebook.png'),
+                    ],
+                  ),
+            
+                  const SizedBox(height: 30),
+                  
+                  //not a member
+            
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
                       Text(
-                        'Or Continue with',
+                        'Not a Member?',
                         style: TextStyle(
-                          color: Colors.grey.shade600,
+                          fontSize: 20,
+                        ),
+                        ),
+                      SizedBox(width: 10),
+                      Text(
+                        'Register Now',
+                        style: TextStyle(
+                          color: Colors.blue,
                           fontWeight: FontWeight.bold,
                           fontSize: 20
                         ),
-                        ),
-                      const Expanded(
-                        child: Divider(
-                          thickness: 1.2,
-                          color: Colors.grey,
-                        ),
-                        ),
-                  ],
-                ),
-                const SizedBox(height: 30),
-                //google and apple image
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    //google button
-                    SquareTile(imagepath: 'images/google.png'),
-
-                    SizedBox(width: 20),
-
-                    //apple image
-                    SquareTile(imagepath: 'images/facebook.png'),
-                  ],
-                ),
-
-                const SizedBox(height: 30),
-                
-                //not a member
-
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Not a Member?',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                      ),
-                    SizedBox(width: 10),
-                    Text(
-                      'Register Now',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20
-                      ),
-
+            
+                      )
+            
+            
+                    ],
                     )
-
-
-                  ],
-                  )
-
-              
-            ]
-            ),
+            
+                
+              ]
+              ),
+          ),
         ),
       ),
     );
