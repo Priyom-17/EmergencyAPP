88888c1722048fb885bb863e4798b4135cbc311b
test-demo <rounok107@gmail.com>
2024-01-23 23:02:52 +0600

    home page implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,27 @@
-import 'package:emergency_app/auth_page.dart';
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+// ignore_for_file: prefer_const_constructors
+import "package:flutter/material.dart";
+import 'package:google_fonts/google_fonts.dart';
+import "package:assignment_saleheen/home_screen.dart";
 
+void main() {
   runApp(const MyApp());
-
-  //test
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AuthPage(),
-    );
+    return MaterialApp(
+        title: 'Flutter Demo',
+        // scaffoldMessengerKey: navigatorkey,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          textTheme: GoogleFonts.firaSansTextTheme(
+            Theme.of(context).textTheme,
+          ),
+          
+          primarySwatch: Colors.blue,
+        ),
+        home: HomeScreen());
   }
-  }
-
+}
